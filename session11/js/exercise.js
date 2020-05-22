bf08a01db2bec465d28d6ce7f1fc94deae4177a4
JIN <a1824jj@aiit.ac.jp>
2020-05-22 09:33:30 +0000

    event & event handler - sample

---
+++ session11/js/exercise.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+  
+  var e = document.getElementById('button');
+  e.addEventListener('click', function(){
+    appendElement();
+  }, false);
+
+},false);
+
+function appendElement(){
+  var result = document.getElementById('result');
+  var li = document.createElement('li');
+  var text = document.createTextNode(result.value);
+  li.appendChild(text);
+
+  var listsElement = document.getElementById('lists');
+  listsElement.appendChild(li);
+}
