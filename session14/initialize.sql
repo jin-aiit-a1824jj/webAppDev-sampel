ad7a9667111e009120dae1434ed2628136fab4ec
JIN <a1824jj@aiit.ac.jp>
2020-05-25 03:27:17 +0000

    using sql-file & initialize

---
+++ session14/initialize.sql
@@ -0,0 +1,5 @@
+drop user if exists mydbuser@localhost;
+create user mydbuser@localhost identified by 'PassW0rd';
+drop database if exists mydb;
+create database mydb;
+grant all on mydb.* to mydbuser@localhost with grant option;
