fa1bbd851e623dbb5e72e11ea66a9c513d508fab
JIN <a1824jj@aiit.ac.jp>
2020-05-26 01:52:35 +0000

    implement controller - destroy function

--- session15/todo/app/controllers/tasks_controller.rb
+++ session15/todo/app/controllers/tasks_controller.rb
@@ -32,6 +32,12 @@ class TasksController < ApplicationController
 
   end
 
+  def destroy 
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
   private
     def task_params
       params[:task].permit(:title)
