bc880053e74f2cec03802b61a77635880fd0aaf6
JIN <a1824jj@aiit.ac.jp>
2020-05-26 01:35:54 +0000

    implement controller - new & create function

--- session15/todo/app/controllers/tasks_controller.rb
+++ session15/todo/app/controllers/tasks_controller.rb
@@ -4,5 +4,22 @@ class TasksController < ApplicationController
     @tasks = Task.all 
   end
 
+  def new
+    @task = Task.new 
+  end
+
+  def create 
+    @task = Task.new(task_params)
+    if @task.save 
+      redirect_to root_path
+    else 
+      render 'new'
+    end
+  end
+
+  private
+    def task_params
+      params[:task].permit(:title)
+    end
 
 end
